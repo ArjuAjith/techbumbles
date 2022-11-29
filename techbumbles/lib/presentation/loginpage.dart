b44992e4e831374ebd067dea22bd043e2984c1f3
ArjuAjith <ajithadhas007@gmail.com>
2022-11-29 09:35:00 +0530

    done login page

--- techbumbles/lib/presentation/loginpage.dart
+++ techbumbles/lib/presentation/loginpage.dart
@@ -1,10 +1,132 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:techbumbles/model/auth_data.dart';
+import 'package:techbumbles/presentation/welcome_screen.dart';
+import 'package:techbumbles/repo.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController userNameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool updating = false;
+  AuthData? authData;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 30,
+            child: TextField(
+              keyboardType: TextInputType.name,
+              controller: userNameController,
+              decoration: InputDecoration(
+                  hintText: "Username", border: InputBorder.none),
+            ),
+          ),
+          SizedBox(height: 20),
+          Container(
+            height: 30,
+            child: TextField(
+              obscureText: true,
+              keyboardType: TextInputType.visiblePassword,
+              controller: passwordController,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Password",
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 48,
+            width: MediaQuery.of(context).size.width,
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(16)))),
+                onPressed: () async {
+                  FocusScope.of(context).unfocus();
+                  if (!updating) {
+                    if (validateLoginfield(userNameController.text.trim(),
+                        passwordController.text.trim())) {
+                      authData = await DataService()
+                          .doLogin(username: "kminchelle", password: "0lelplR");
+                      if (authData == null) {
+                        ScaffoldMessenger.of(context)
+                          ..clearSnackBars()
+                          ..showSnackBar(
+                              SnackBar(content: Text("Someting went wrong!!")));
+                      }
+                      if (authData != null) {
+                        userNameController.clear();
+                        passwordController.clear();
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => WelcomePage()));
+                      }
+                      setState(() {
+                        updating = false;
+                      });
+                    }
+                    // print(authData.email);
+                  }
+                  if (updating) {
+                    () {};
+                  }
+                },
+                child: updating
+                    ? CupertinoActivityIndicator()
+                    : Text(
+                        "Login",
+                        style:
+                            TextStyle(color: Color.fromARGB(255, 71, 59, 74)),
+                      )),
+          )
+        ],
+      ),
+    ));
+  }
+
+  bool validateLoginfield(String username, String password) {
+    setState(() {
+      updating = true;
+    });
+    if (username.isEmpty) {
+      ScaffoldMessenger.of(context)
+        ..clearSnackBars()
+        ..showSnackBar(SnackBar(
+            content: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text("Please Enter the UserName"),
+        )));
+      updating = false;
+      return false;
+    }
+    if (password.isEmpty) {
+      ScaffoldMessenger.of(context)
+        ..clearSnackBars()
+        ..showSnackBar(SnackBar(
+            content: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text("Please Enter the Password"),
+        )));
+      updating = false;
+      return false;
+    }
+    return true;
   }
 }
---
+++ techbumbles/lib/repo.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:techbumbles/model/auth_data.dart';
+
+class DataService {
+  Future<AuthData?> doLogin(
+      {required String username, required String password}) async {
+    var url = 'https://dummyjson.com/auth/login';
+    var body = jsonEncode({"username": username, "password": password});
+
+    http.Response response = await http.post(Uri.parse(url),
+        headers: {"Content-Type": "application/json"}, body: body);
+    // print(response.request);
+    print(response.statusCode);
+    // print(response.body);
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      return AuthData.fromJson(json);
+    }
+  }
+}
+
+// Future<http.Response> postRequest() async {
+//   var url =
+//       'https://pae.ipportalegre.pt/testes2/wsjson/api/app/ws-authenticate';
+//   var body = jsonEncode({
+//     'data': {'apikey': '12345678901234567890'}
+//   });
+
+//   print("Body: " + body);
+
+//   http
+//       .post(url, headers: {"Content-Type": "application/json"}, body: body)
+//       .then((http.Response response) {
+//     print("Response status: ${response.statusCode}");
+//     print("Response body: ${response.contentLength}");
+//     print(response.headers);
+//     print(response.request);
+//   });
+// }
